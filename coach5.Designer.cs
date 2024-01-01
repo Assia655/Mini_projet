74f83063d9cf6fc9c503a73cc6e91228bcd85811
nihal baidaoui <nihalbaidaoui12@gmail.com>
2024-01-01 22:47:59 +0100

    esea

--- coach5.Designer.cs
+++ coach5.Designer.cs
@@ -68,7 +68,7 @@
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.label1.Location = new System.Drawing.Point(327, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(329, 39);
+            this.label1.Size = new System.Drawing.Size(318, 38);
             this.label1.TabIndex = 1;
             this.label1.Text = "Chercher un Coach";
             // 
@@ -93,7 +93,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.DarkOrange;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(540, 261);
+            this.button1.Location = new System.Drawing.Point(539, 261);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(137, 36);
             this.button1.TabIndex = 4;
