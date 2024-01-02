664ee03059577474e53c47a3cded376f376ee582
nihal baidaoui <nihalbaidaoui12@gmail.com>
2024-01-02 12:53:36 +0100

    fdhny

--- Coach22.Designer.cs
+++ Coach22.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(290, 203);
+            this.button1.Location = new System.Drawing.Point(399, 183);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 76);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
